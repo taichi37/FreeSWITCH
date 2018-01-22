dc3618514f3c53e7c1ddb4bcb77b5cb96fc385e3
TaiChi <jesse.guanyu@gmail.com>
2018-01-22 15:59:19 +0800

    Create door.lua

---
+++ doorphone/door.lua
@@ -0,0 +1,24 @@
+r = io.open( "/dev/ttyUSB0", "r" );
+ch1 = r:read();
+r:flush();
+
+function sleep( s )
+    os.execute( "sleep " .. s );
+end
+
+while 1 do
+    local ch = r:read(); 
+    r:flush();
+    
+    if ch == "1000" then
+        os.execute( "linphonecsh dial 1016" );
+    end
+
+    if ch == "1018" then
+        os.execute( "linphonecsh dial 1002" );
+    end
+
+    sleep( 0.1 );
+end
+
+-- INSERT --                                                  15,7          All
